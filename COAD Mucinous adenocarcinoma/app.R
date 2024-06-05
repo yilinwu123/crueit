d91bf87385a4a596ea536f1479f93aece9e5d645
yilin123 <59527986+yilinwu123@users.noreply.github.com>
2024-06-05 20:44:06 +0800

    Update app.R

--- COAD Mucinous adenocarcinoma/app.R
+++ COAD Mucinous adenocarcinoma/app.R
@@ -68,7 +68,7 @@ colnames(datausefinal)[2]='os_status'
 
 
 #dataplot=read.csv(paste('/users/yilin/Downloads/cureitsurvival-2/cure',m,n,'.csv'))
-dataplot=read.csv(paste('/users/yilin/Downloads/cureitsurvival/cure',m,n,'.csv'))
+dataplot=read.csv(paste('data/cure',m,n,'.csv'))
 dataplot$color[dataplot$scoretest<0.01]=1
 dataplot$color[dataplot$scoretest>=0.01]=0
 dataplot$color=as.factor(dataplot$color)
